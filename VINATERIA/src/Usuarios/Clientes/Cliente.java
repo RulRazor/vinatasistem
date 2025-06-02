033bfbd271d27738cd5fe8b7030bf740dd39c0ea
RulRazor <raulhernandezguerrero2266@gmail.com>
2025-06-02 10:44:58 -0600

    Se agrego metodos para guardar el cliente que se este atendiendo en ese momento

--- VINATERIA/src/Usuarios/Clientes/Cliente.java
+++ VINATERIA/src/Usuarios/Clientes/Cliente.java
@@ -3,13 +3,27 @@ package Usuarios.Clientes;
 import Usuarios.Usuario;
 
 public abstract class Cliente extends Usuario {
+    private static String clienteActual;
     public Cliente(String apellido, String funcion, String ID, String nombre, String password, Boolean status) {
         super(apellido, funcion, ID, nombre, password, status);
     }
 
 
+    //Metodos hereditarios, para guardar el usuario
 
-    //Metodos
+    public static void guardarCliente(String usuario) {
+        clienteActual = usuario;
+    }
+
+    public static String getClienteActual() {
+        return clienteActual;
+    }
+
+    public static void cerrarSesionCliente() {
+        clienteActual = null;
+    }
+
+    //Metodos abstractos
     public abstract void historialCompras();
     public abstract void generarComprobante();
 }
